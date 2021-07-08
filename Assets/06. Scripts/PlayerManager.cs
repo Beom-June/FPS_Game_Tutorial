2473238ddc2ec312c954fbd981661459316e2a31
Beom-June <zxo9988@naver.com>
2021-07-08 17:56:56 +0900

    Add
    Save

---
+++ Assets/06. Scripts/PlayerManager.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    // 플레이어 설정
+    public float hitPoint = 100f;
+    private float maxHitPoint;                                  // 체력
+
+    // 레퍼런스
+    public Text hpText;                                         // hp text 담는 변수
+    public RectTransform hpBar;                                 // hpBar 설정 
+
+    void Start()
+    {
+        maxHitPoint = hitPoint;
+        hpText.text = hitPoint + " / " + maxHitPoint;
+        hpBar.localScale = Vector3.one;
+    }
+
+
+    // Debug
+    void Update()
+    {
+        // 디버그를 위해서 k를 눌렀을 때 1 ~ 20의 랜덤 데미지가 들어오도록 설정
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            ApplyDamage(Random.Range(1, 20));
+        }
+    }
+
+    public void ApplyDamage(float damage)
+    {
+        UpdateHP();
+        hitPoint -= damage;
+        if(hitPoint <= 0)
+        {
+            hitPoint = 0;
+
+            // Die;
+            Debug.Log("플레이어 죽음");
+        }
+    }
+
+    // 데미지가 들어와서 깎인 체력 만큼 hpText를 변경, hpBar 크기 줄임
+    void UpdateHP()
+    {
+        hpText.text = hitPoint + " / " + maxHitPoint;
+        hpBar.localScale = new Vector3(hitPoint / maxHitPoint, 1, 1);
+    }
+}
--- Assets/06. Scripts/Weapon.cs
+++ Assets/06. Scripts/Weapon.cs
@@ -6,15 +6,18 @@ using UnityEngine.UI;
 public class Weapon : MonoBehaviour
 {
     [Header("Weapon Specification")]
-    public string weaponName;                                    // 무기 이름
+    public string weaponName;                                   // 무기 이름
     public int bulletsPerMag;                                   // 한 탄창의 탄환 수
     public int bulletsTotal;                                    // 최대 총알 갯수
     public int currentBullets;                                  // 현재 총알
+    public float damage;                                        // 총의 데미지
     public float range;                                         // 사거리
     public float fireRate;                                      // 딜레이
-    public Vector3 aimPostion;                                  // 조준 위치
-    public Vector3 originalPosition;                            // 원래 위치
+    public float accuracy;                                      // 총의 집탄율 조정
 
+    public Vector3 aimPostion;                                  // 조준 위치
+    private Vector3 originalPosition;                            // 원래 위치
+    private float originalAccuracy;                             // 원래 집탄율
 
     // 파라미터 선언
     private float fireTimer;                                    // 파라미터. 총 쏘는 시간
@@ -22,16 +25,17 @@ public class Weapon : MonoBehaviour
     private bool isAiming;                                      // 조준하고 있는지 아닌지를 판별
 
     // 레퍼런스 선언
+    public Transform shootPoint;                                // 격발 위치
+    public Transform bulletCasingPoint;                         // 총알 나오는 위치 변수
     private Animator anim;                                      // 애니메이터 설정
     public ParticleSystem muzzleFlash;                          // 격발위치 플래시 설정
     public Text bulletsText;                                    // 총알 텍스트 담는 변수
 
     // 프리팹 담는 변수
     public GameObject hitSparkPrefab;                           // 히트 파티클 넣는 프리팹 변수
-    public GameObject hitHolePrefab;                           // 히트홀 넣는 프리팹 변수
+    public GameObject hitHolePrefab;                            // 히트홀 넣는 프리팹 변수
+    public GameObject bulletCasing;
 
-
-    public Transform shootPoint;                                // 격발 위치
 
     // 사운드 담는 변수
     public AudioSource audioSource;                             // 사운드 설정
@@ -41,7 +45,8 @@ public class Weapon : MonoBehaviour
     // 반동 주는 변수
     public Transform camRecoil;                                 // 카메라와 무기에 반동
     public Vector3 recoilKickback;                              // 원위치
-    public float recoilAmount;
+    public float recoilAmount;                                  // 반동의 세기
+    public float originalRecoil;                                // 원래 반동
     
 
     void Start()
@@ -49,7 +54,10 @@ public class Weapon : MonoBehaviour
         currentBullets = bulletsPerMag;
         anim = GetComponent<Animator>();
         bulletsText.text = currentBullets + " / " + bulletsTotal;
+
         originalPosition = transform.localPosition;
+        originalAccuracy = accuracy;
+        originalRecoil = recoilAmount;
     }
 
     void Update()
@@ -96,26 +104,47 @@ public class Weapon : MonoBehaviour
 
     void Fire()
     {
-        if(fireTimer < fireRate)
+        if(fireTimer < fireRate || isReloading)
         {
             return;
         }
-        Debug.Log("격발");
+        //Debug.Log("격발");
 
         // 레이 캐스트
         RaycastHit raycastHit;
-        if(Physics.Raycast (shootPoint.position, shootPoint.transform.forward, out raycastHit, range))
+
+        // Random.onUnitSphere는 직경이 1인 구안에 있는 랜덤한 위치 반환. 
+        // accuracy 값을 조절하면 원하는 산탄기능 얻을 수 있음. 낮을 수록 정확함.
+        if(Physics.Raycast (shootPoint.position, shootPoint.transform.forward + Random.onUnitSphere * accuracy, out raycastHit, range))
         {
             //Debug.Log("맞음");
             // hitSpark 프리팹 생성. Quaternion.FromToRotation : 축을 방향으로 회전
             GameObject hitSpark = Instantiate(hitHolePrefab, raycastHit.point, Quaternion.FromToRotation(Vector3.up, raycastHit.normal));
+            // 피격된 물체가 부모 오브젝트가 되게 함
+            hitSpark.transform.SetParent(hitSpark.transform);
             // 0.5초 후에 hitSpark 삭제
             Destroy(hitSpark, 0.5f);
 
             // hitHole 프리팹 생성.
             GameObject hitHole = Instantiate(hitHolePrefab, raycastHit.point, Quaternion.FromToRotation(Vector3.up, raycastHit.normal));
-            // 3초 후에 hitHole 삭제
-            Destroy(hitHole, 3f);
+            // 피격된 물체가 부모 오브젝트가 되게 함
+            hitHole.transform.SetParent(hitHole.transform);
+            // 4초 후에 hitHole 삭제
+            Destroy(hitHole, 4f);
+
+            // 적에게 데미지를 줌
+            HealthManager healthManager = raycastHit.transform.GetComponent<HealthManager>();
+            if(healthManager)
+            {
+                healthManager.ApplyDamage(damage);
+            }
+
+            // 피격되는 물체에 Rigidbody 컴포넌트가 존재하면. AddForceAtPosition를 이용 -> 데미지에 비례하여 위치에서 힘을 줌
+            Rigidbody rigidbody = raycastHit.transform.GetComponent<Rigidbody>();
+            if(rigidbody)
+            {
+                rigidbody.AddForceAtPosition(transform.forward * 5f * damage, transform.position);
+            }
         }
         // 격발 되어서 총알 감소
         currentBullets--;
@@ -132,6 +161,8 @@ public class Weapon : MonoBehaviour
 
         // 반동 호출
         Recoil();
+        // 탄약 이펙트 함수 호출
+        BulletEffect();
     }
 
     // 재장전
@@ -167,13 +198,19 @@ public class Weapon : MonoBehaviour
         {
             transform.localPosition = Vector3.Lerp(transform.localPosition, aimPostion, Time.deltaTime * 8f);
             Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 40f, Time.deltaTime * 8f);
+
             isAiming = true;
+            accuracy = originalAccuracy / 2f;
+            recoilAmount = originalRecoil / 2f;
         }
         else
         {
             transform.localPosition = Vector3.Lerp(transform.localPosition, aimPostion, Time.deltaTime * 5f);
             Camera.main.fieldOfView = Mathf.Lerp(Camera.main.fieldOfView, 60f, Time.deltaTime * 8f);
+
             isAiming = false;
+            accuracy = originalAccuracy;
+            recoilAmount = originalRecoil;
         }
     }
 
@@ -196,5 +233,18 @@ public class Weapon : MonoBehaviour
     void RecoilBack()
     {
         camRecoil.localRotation = Quaternion.Slerp(camRecoil.localRotation, Quaternion.identity, Time.deltaTime * 2f);
+    }
+
+    // 총알 이펙트
+    void BulletEffect()
+    {
+        Quaternion randomQuaternion = new Quaternion(Random.Range(0, 360f), Random.Range(0, 360f), Random.Range(0, 360f), 1);
+
+        GameObject casing = Instantiate(bulletCasing, bulletCasingPoint);
+        casing.transform.localRotation = randomQuaternion;
+        casing.GetComponent<Rigidbody>().AddRelativeForce(new Vector3(Random.Range(50f, 100f), Random.Range(50f, 100f), Random.Range(-30f, 30f)));
+
+        // 케이싱 파괴
+        Destroy(casing, 1f);
     }
 }
